318b576cead429c7c4cea1c3f5a0d043417760f8
mabelenda <mabelenda@itba.edu.ar>
2018-04-20 14:11:18 -0300

    gridlock rules

--- tp2/src/main/java/ar/edu/itba/sia/gridLock/GridLockProblem.java
+++ tp2/src/main/java/ar/edu/itba/sia/gridLock/GridLockProblem.java
@@ -1,5 +1,6 @@
 package ar.edu.itba.sia.gridLock;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import ar.com.itba.sia.Problem;
@@ -20,7 +21,20 @@ public class GridLockProblem implements Problem<GridLockState> {
 
     @Override
     public List<Rule<GridLockState>> getRules(GridLockState state) {
-      return null;
+
+        List<Rule<GridLockState>> rules = new LinkedList<>();
+
+        for(GridLockPiece p : state.getPieces()) {
+            switch (p.getType()) {
+                case VERTICAL:
+                    rules.addAll(verticalMove(p,state.getBoard()));
+                    break;
+                case HORIZONTAL:
+                    rules.addAll(horizontalMove(p,state.getBoard()));
+            }
+        }
+
+      return rules;
     }
 
     @Override
@@ -31,5 +45,60 @@ public class GridLockProblem implements Problem<GridLockState> {
         return mainPiece.getPosition().getX() + mainPiece.getSize() == initialState.getBoard().getSize();
     }
 
+    private List<Rule<GridLockState>> verticalMove(final GridLockPiece piece, final Board board) {
+
+        List<Rule<GridLockState>> rules = new LinkedList<>();
+
+        int pieceStart = piece.getPosition().getY();
+
+        for(int offset = -1, i = 0 ; i < pieceStart ; i++, offset--) {
+            if(!board.getMatrix().get(piece.getPosition().getX(),piece.getPosition().getY() + offset)) {
+                rules.add(new GridLockRule(piece, offset));
+            } else {
+                break;
+            }
+
+        }
+
+        int boardHeight = board.getMatrix().getHeight()-1;
+
+        for(int offset = 1, i = piece.getPosition().getY() + piece.getSize() ; i < boardHeight ; i++, offset ++) {
+            if (!board.getMatrix().get(piece.getPosition().getX(),piece.getPosition().getY() + piece.getSize() + offset)) {
+                rules.add(new GridLockRule(piece, offset));
+            } else {
+                break;
+            }
+        }
+
+        return rules;
+    }
+
+    private List<Rule<GridLockState>> horizontalMove(final GridLockPiece piece, final Board board) {
+
+        List<Rule<GridLockState>> rules = new LinkedList<>();
+
+        int pieceStart = piece.getPosition().getX();
+
+        for(int offset = -1, i = 0 ; i < pieceStart ; i++, offset--) {
+            if(!board.getMatrix().get(piece.getPosition().getX() + offset, piece.getPosition().getY())) {
+                rules.add(new GridLockRule(piece, offset));
+            } else {
+                break;
+            }
+
+        }
+
+        int boardWidth = board.getMatrix().getWidth()-1;
+
+        for(int offset = 1, i = piece.getPosition().getX() + piece.getSize() ; i < boardWidth ; i++, offset ++) {
+            if (!board.getMatrix().get(piece.getPosition().getX() + piece.getSize() + offset, piece.getPosition().getY())) {
+                rules.add(new GridLockRule(piece, offset));
+            } else {
+                break;
+            }
+        }
+
+        return rules;
+    }
 
 }
