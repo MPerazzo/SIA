64833827e6ba668d6ea037170d6714f5b7da808d
matias <maperazzo@itba.edu.ar>
2018-04-21 16:10:46 -0300

    programación orientada a interfaces

--- tp2/src/main/java/ar/edu/itba/sia/Main.java
+++ tp2/src/main/java/ar/edu/itba/sia/Main.java
@@ -4,6 +4,7 @@ package ar.edu.itba.sia;
 import ar.edu.itba.sia.core.SearchEngine;
 import ar.edu.itba.sia.gridLock.GridLockProblem;
 import ar.edu.itba.sia.gridLock.GridLockState;
+import ar.edu.itba.sia.interfaces.SearchAlgorithm;
 import ar.edu.itba.sia.searchAlgorithms.BreadthFirstSearch;
 import ar.edu.itba.sia.utils.ConfigurationManager;
 
@@ -13,7 +14,7 @@ public class Main {
 
         SearchEngine<GridLockState> searchEngine = new SearchEngine<>();
 
-        BreadthFirstSearch<GridLockState> searchAlgorithm = new BreadthFirstSearch<>();
+        SearchAlgorithm<GridLockState> searchAlgorithm = new BreadthFirstSearch<>();
 
         GridLockProblem p = new GridLockProblem(ConfigurationManager.getInstance().getInitialState());
 
