056066cd7d5bf39856319a563157de79689a3f17
matias <maperazzo@itba.edu.ar>
2018-04-30 01:46:55 -0300

    mergeando con master

---
+++ tp2/src/main/java/ar/edu/itba/sia/interfaces/InformedSearchAlgorithm.java
@@ -0,0 +1,4 @@
+package ar.edu.itba.sia.interfaces;
+
+public interface InformedSearchAlgorithm<T> extends SearchAlgorithm<T> {
+}
---
+++ tp2/src/main/java/ar/edu/itba/sia/interfaces/UnInformedSearchAlgorithm.java
@@ -0,0 +1,5 @@
+package ar.edu.itba.sia.interfaces;
+
+
+public interface UnInformedSearchAlgorithm<T> extends SearchAlgorithm<T> {
+}
