f3b4701662dbd721d477faeca11aeb8d3231dba2
matias <maperazzo@itba.edu.ar>
2018-05-01 12:45:29 -0300

    renombrando casos

--- tp2/src/main/java/ar/edu/itba/sia/utils/ConfigurationManager.java
+++ tp2/src/main/java/ar/edu/itba/sia/utils/ConfigurationManager.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 public class ConfigurationManager {
 
-    private final static String fileName = "caseMedium.txt";
+    private final static String fileName = "caseB.txt";
     private final Parser parser;
     private final GridLockState initialState;
 
