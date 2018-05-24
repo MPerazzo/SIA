85609d33eac5ff6f1dcdbaaba3147a8b130a36f9
matias <maperazzo@itba.edu.ar>
2018-05-24 13:28:39 -0300

    main listo para correr

--- tp3/src/main/java/ar/edu/itba/sia/Main.java
+++ tp3/src/main/java/ar/edu/itba/sia/Main.java
@@ -1,14 +1,20 @@
 package ar.edu.itba.sia;
 
 
+import ar.edu.itba.sia.core.GeneticAlgorithm;
+import ar.edu.itba.sia.model.character.Character;
+import ar.edu.itba.sia.model.character.warrior.Warrior3;
+import ar.edu.itba.sia.utils.Parser;
+
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main
 {
-    public static void main( String[] args ) throws IOException {
-
-
-
+    public static void main( String[] args )  {
+        GeneticAlgorithm g = new GeneticAlgorithm(new Parser("configFile.txt"));
+        g.geneticAlgorithm();
     }
 
 }
