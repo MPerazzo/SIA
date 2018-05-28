d13dbd506a1cf8df1b4e173112a58eac34a05686
matias <maperazzo@itba.edu.ar>
2018-05-28 12:45:11 -0300

    12 hilos

--- tp3/src/main/java/ar/edu/itba/sia/core/Raid.java
+++ tp3/src/main/java/ar/edu/itba/sia/core/Raid.java
@@ -38,20 +38,57 @@ public class Raid {
         GeneticAlgorithm g5 = new GeneticAlgorithm(new Parser("configFile5.txt", armors, boots, gloves,
                 helmets, weapons));
 
-            Future<Void> f1 = e.submit(() -> g1.geneticAlgorithm());
-            Future<Void> f2 = e.submit(() -> g2.geneticAlgorithm());
-            Future<Void> f3 = e.submit(() -> g3.geneticAlgorithm());
-            Future<Void> f4 = e.submit(() -> g4.geneticAlgorithm());
-            Future<Void> f5 = e.submit(() -> g5.geneticAlgorithm());
+        GeneticAlgorithm g6 = new GeneticAlgorithm(new Parser("configFile6.txt", armors, boots, gloves,
+                helmets, weapons));
+
+        GeneticAlgorithm g7 = new GeneticAlgorithm(new Parser("configFile7.txt", armors, boots, gloves,
+                helmets, weapons));
+
+        GeneticAlgorithm g8 = new GeneticAlgorithm(new Parser("configFile8.txt", armors, boots, gloves,
+                helmets, weapons));
+
+        GeneticAlgorithm g9 = new GeneticAlgorithm(new Parser("configFile9.txt", armors, boots, gloves,
+                helmets, weapons));
+
+        GeneticAlgorithm g10 = new GeneticAlgorithm(new Parser("configFile10.txt", armors, boots, gloves,
+                helmets, weapons));
+
+        GeneticAlgorithm g11 = new GeneticAlgorithm(new Parser("configFile11.txt", armors, boots, gloves,
+                helmets, weapons));
+
+        GeneticAlgorithm g12 = new GeneticAlgorithm(new Parser("configFile12.txt", armors, boots, gloves,
+                helmets, weapons));
+
+        Future<Void> f1 = e.submit(() -> g1.geneticAlgorithm());
+        Future<Void> f2 = e.submit(() -> g2.geneticAlgorithm());
+        Future<Void> f3 = e.submit(() -> g3.geneticAlgorithm());
+        Future<Void> f4 = e.submit(() -> g4.geneticAlgorithm());
+        Future<Void> f5 = e.submit(() -> g5.geneticAlgorithm());
+        Future<Void> f6 = e.submit(() -> g6.geneticAlgorithm());
+        Future<Void> f7 = e.submit(() -> g7.geneticAlgorithm());
+        Future<Void> f8 = e.submit(() -> g8.geneticAlgorithm());
+        Future<Void> f9 = e.submit(() -> g9.geneticAlgorithm());
+        Future<Void> f10 = e.submit(() -> g10.geneticAlgorithm());
+        Future<Void> f11 = e.submit(() -> g11.geneticAlgorithm());
+        Future<Void> f12 = e.submit(() -> g12.geneticAlgorithm());
 
 
-            f1.get();
-            f2.get();
-            f3.get();
-            f4.get();
-            f5.get();
+        f1.get();
+        f2.get();
+        f3.get();
+        f4.get();
+        f5.get();
+        f6.get();
+        f7.get();
+        f8.get();
+        f9.get();
+        f10.get();
+        f11.get();
+        f12.get();
 
-            e.shutdown();
+        e.shutdown();
+
+
 
 
         /*
